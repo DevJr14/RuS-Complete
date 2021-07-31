e151b7a11bbe22870d8d49ebd45b4e3caee7b53b
Junior Matlou <Junianohmm6@gmail.com>
2021-07-31 18:27:35 +0200

    Added command for adding or editing company and its validator.

---
+++ src/Application/Features/Companies/Commands/AddEdit/AddEditCompanyCommand.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Localization;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Core;
+using RuS.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Companies.Commands.AddEdit
+{
+    public class AddEditCompanyCommand : IRequest<Result<int>>
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Company name is required.")]
+        public string Name { get; set; }
+        public string ShortTitle { get; set; }
+        public string RegistrationNo { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+    }
+
+    public class AddEditCompanyCommandHnadler : IRequestHandler<AddEditCompanyCommand, Result<int>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly IStringLocalizer<AddEditCompanyCommandHnadler> _localizer;
+
+        public AddEditCompanyCommandHnadler(IStringLocalizer<AddEditCompanyCommandHnadler> localizer, IUnitOfWork<int> unitOfWork, IMapper mapper)
+        {
+            _localizer = localizer;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<int>> Handle(AddEditCompanyCommand command, CancellationToken cancellationToken)
+        {
+            if (command.Id == 0)
+            {
+                var company = _mapper.Map<Company>(command);
+                await _unitOfWork.Repository<Company>().AddAsync(company);
+                await _unitOfWork.Commit(cancellationToken);
+                return await Result<int>.SuccessAsync(company.Id, "Company Saved");
+            }
+            else
+            {
+                var company = await _unitOfWork.Repository<Company>().GetByIdAsync(command.Id);
+                if (company != null)
+                {
+                    company.Name = command.Name ?? company.Name;
+                    company.ShortTitle = command.ShortTitle ?? company.ShortTitle;
+                    company.RegistrationNo = command.RegistrationNo ?? company.RegistrationNo;
+                    company.RegistrationDate = command.RegistrationDate ?? company.RegistrationDate;
+                    await _unitOfWork.Repository<Company>().UpdateAsync(company);
+                    await _unitOfWork.Commit(cancellationToken);
+                    return await Result<int>.SuccessAsync(company.Id, "Company Updated");
+                }
+                else
+                {
+                    return await Result<int>.FailAsync("Company Not Found!");
+                }
+            }
+        }
+    }
+}
---
+++ src/Application/Validators/Features/Companies/Commands/AddEdit/AddEditCompanyCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using RuS.Application.Features.Companies.Commands.AddEdit;
+using RuS.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Validators.Features.Companies.Commands.AddEdit
+{
+    public class AddEditCompanyCommandValidator : AbstractValidator<AddEditCompanyCommand>
+    {
+        private readonly ICompanyRepository _companyRepository;
+        public AddEditCompanyCommandValidator(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+
+            RuleFor(c => c)
+                .MustAsync(UniqueEntry).WithMessage("Company already exists.");
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Company name cannot be empty.")
+                .MaximumLength(30).WithMessage("Company name must not exceed 30 characters.");
+        }
+
+        private async Task<bool> UniqueEntry(AddEditCompanyCommand command, CancellationToken cancelationToken)
+        {
+            return await _companyRepository.IsUniqueEntry(command.Name, command.RegistrationNo, command.Id);
+        }
+    }
+}
