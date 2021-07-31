a9212141076c235f3ceb0b490cdb5e677c0b2c59
Junior Matlou <Junianohmm6@gmail.com>
2021-07-31 18:32:42 +0200

    Added company mapping profile

---
+++ src/Application/Mappings/CompanyProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RuS.Application.Features.Companies.Commands.AddEdit;
+using RuS.Domain.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Mappings
+{
+    public class CompanyProfile : Profile
+    {
+        public CompanyProfile()
+        {
+            CreateMap<AddEditCompanyCommand, Company>().ReverseMap();
+        }
+    }
+}
