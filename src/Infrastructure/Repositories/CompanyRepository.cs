536c0093e6ecd948fa9d7c791c7c09b6a6d77903
Junior Matlou <Junianohmm6@gmail.com>
2021-07-31 18:34:56 +0200

    Added company repo and its implementation class

---
+++ src/Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Infrastructure.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly IRepositoryAsync<Company, int> _repository;
+
+        public CompanyRepository(IRepositoryAsync<Company, int> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> IsUniqueEntry(string name, string regNo, int id = 0)
+        {
+            List<Company> companies = await _repository.Entities.ToListAsync();
+            if(id == 0)
+            {
+                return !companies.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase)
+                    && string.Equals(c.RegistrationNo, regNo, StringComparison.OrdinalIgnoreCase));
+            }
+            else
+            {
+                return !companies.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase)
+                    && string.Equals(c.RegistrationNo, regNo, StringComparison.OrdinalIgnoreCase)
+                    && c.Id != id);
+            }
+        }
+    }
+}
