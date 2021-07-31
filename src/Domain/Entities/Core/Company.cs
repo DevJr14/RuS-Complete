9f62aa8aeaf1ebc29507176b4349e34bc8300df6
Junior Matlou <Junianohmm6@gmail.com>
2021-07-31 18:26:01 +0200

    Added Company Entity

---
+++ src/Domain/Entities/Core/Company.cs
@@ -0,0 +1,20 @@
+﻿using RuS.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Domain.Entities.Core
+{
+    public class Company : AuditableEntity<int>
+    {
+        public string Name { get; set; }
+
+        public string ShortTitle { get; set; }
+
+        public string RegistrationNo { get; set; }
+
+        public DateTime? RegistrationDate { get; set; }
+    }
+}
