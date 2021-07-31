8c7bed8a99b05984dbd63ead6fddac28c1a5d067
Junior Matlou <Junianohmm6@gmail.com>
2021-07-31 18:38:00 +0200

    Adds company manager interface.

--- src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -29,6 +29,7 @@ namespace RuS.Infrastructure.Extensions
                 .AddTransient<IProductRepository, ProductRepository>()
                 .AddTransient<IBrandRepository, BrandRepository>()
                 .AddTransient<IDocumentRepository, DocumentRepository>()
+                .AddTransient<ICompanyRepository, CompanyRepository>()
                 .AddTransient<IDocumentTypeRepository, DocumentTypeRepository>()
                 .AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
         }
