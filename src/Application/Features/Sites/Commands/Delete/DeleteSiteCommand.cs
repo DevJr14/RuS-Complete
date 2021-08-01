191d9dba3f25ec9e93f200565330ed87969d4d0c
Junior Matlou <Junianohmm6@gmail.com>
2021-08-01 13:55:14 +0200

    Added commands for sites and a validator

---
+++ src/Application/Features/Sites/Commands/Delete/DeleteSiteCommand.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.Extensions.Localization;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Core;
+using RuS.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Sites.Commands.Delete
+{
+    public class DeleteSiteCommand : IRequest<Result<int>>
+    {
+        public int Id { get; set; }
+    }
+
+    internal class DeleteSiteCommandHandler : IRequestHandler<DeleteSiteCommand, Result<int>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly IStringLocalizer<DeleteSiteCommandHandler> _localizer;
+
+        public DeleteSiteCommandHandler(IUnitOfWork<int> unitOfWork, IStringLocalizer<DeleteSiteCommandHandler> localizer)
+        {
+            _unitOfWork = unitOfWork;
+            _localizer = localizer;
+        }
+
+        public async Task<Result<int>> Handle(DeleteSiteCommand command, CancellationToken cancellationToken)
+        {
+            var site = await _unitOfWork.Repository<Site>().GetByIdAsync(command.Id);
+            if (site != null)
+            {
+                await _unitOfWork.Repository<Site>().DeleteAsync(site);
+                await _unitOfWork.Commit(cancellationToken);
+                return await Result<int>.SuccessAsync(site.Id, _localizer["Site Deleted"]);
+            }
+            else
+            {
+                return await Result<int>.FailAsync(_localizer["Site Not Found!"]);
+            }
+        }
+    }
+}
---
+++ src/Application/Validators/Features/Sites/Commands/AddEdit/AddEditSiteCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using RuS.Application.Features.Sites.Commands.AddEdit;
+using RuS.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Validators.Features.Sites.Commands.AddEdit
+{
+    public class AddEditSiteCommandValidator : AbstractValidator<AddEditSiteCommand>
+    {
+        private readonly ISiteRepository _siteRepository;
+
+        public AddEditSiteCommandValidator(ISiteRepository siteRepository)
+        {
+            _siteRepository = siteRepository;
+            RuleFor(s => s)
+                .MustAsync(UniqueEntry).WithMessage("Site already exists.");
+            RuleFor(s => s.CompanyId)
+                .NotEqual(0).WithMessage("Site must be linked to a Company");
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage("Site name cannot be empty.")
+                .MaximumLength(30).WithMessage("Site name must not exceed 30 characters.");
+            RuleFor(s => s.Description)
+                .MaximumLength(100).WithMessage("Site description must not exceed 100 characters.");
+        }
+
+        private async Task<bool> UniqueEntry(AddEditSiteCommand command, CancellationToken cancelationToken)
+        {
+            return await _siteRepository.IsUniqueEntry(command.Name, command.CompanyId, command.Id);
+        }
+    }
+}
