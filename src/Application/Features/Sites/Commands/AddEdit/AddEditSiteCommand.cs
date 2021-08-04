d0a4ad9940d0620ecd726d9d6e0cc4ef7ea93760
Junior Matlou <Junianohmm6@gmail.com>
2021-08-04 23:17:28 +0200

    Add a check for uniqueness

--- src/Application/Features/Sites/Commands/AddEdit/AddEditSiteCommand.cs
+++ src/Application/Features/Sites/Commands/AddEdit/AddEditSiteCommand.cs
@@ -27,38 +27,57 @@ namespace RuS.Application.Features.Sites.Commands.AddEdit
     public class AddEditSiteCommandHandler : IRequestHandler<AddEditSiteCommand, Result<int>>
     {
         private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly ISiteRepository _siteRepository;
         private readonly IMapper _mapper;
         private readonly IStringLocalizer<AddEditSiteCommandHandler> _localizer;
-        public AddEditSiteCommandHandler(IUnitOfWork<int> unitOfWork, IMapper mapper, IStringLocalizer<AddEditSiteCommandHandler> localizer)
+        public AddEditSiteCommandHandler(IUnitOfWork<int> unitOfWork, IMapper mapper, IStringLocalizer<AddEditSiteCommandHandler> localizer, ISiteRepository siteRepository)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _localizer = localizer;
+            _siteRepository = siteRepository;
         }
 
         public async Task<Result<int>> Handle(AddEditSiteCommand command, CancellationToken cancellationToken)
         {
+            
             if (command.Id == 0)
             {
-                var site = _mapper.Map<Site>(command);
-                await _unitOfWork.Repository<Site>().AddAsync(site);
-                await _unitOfWork.Commit(cancellationToken);
-                return await Result<int>.SuccessAsync(site.Id, _localizer["Site Saved"]);
-            }
-            else
-            {
-                var site = await _unitOfWork.Repository<Site>().GetByIdAsync(command.Id);
-                if (site != null)
+                var isUnique = await _siteRepository.IsUniqueEntry(command.Name, command.CompanyId);
+                if (!isUnique)
                 {
-                    site.Name = command.Name ?? site.Name;
-                    site.Description = command.Description ?? site.Description;
-                    await _unitOfWork.Repository<Site>().UpdateAsync(site);
-                    await _unitOfWork.Commit(cancellationToken);
-                    return await Result<int>.SuccessAsync(site.Id, _localizer["Site Updated"]);
+                    return await Result<int>.FailAsync(_localizer["Site already exists."]);
                 }
                 else
                 {
-                    return await Result<int>.FailAsync("Site Not Found!");
+                    var site = _mapper.Map<Site>(command);
+                    await _unitOfWork.Repository<Site>().AddAsync(site);
+                    await _unitOfWork.Commit(cancellationToken);
+                    return await Result<int>.SuccessAsync(site.Id, _localizer["Site Saved"]);
+                }
+            }
+            else
+            {
+                var isUnique = await _siteRepository.IsUniqueEntry(command.Name, command.CompanyId, command.Id);
+                if (!isUnique)
+                {
+                    return await Result<int>.FailAsync(_localizer["Site already exists."]);
+                }
+                else
+                {
+                    var site = await _unitOfWork.Repository<Site>().GetByIdAsync(command.Id);
+                    if (site != null)
+                    {
+                        site.Name = command.Name ?? site.Name;
+                        site.Description = command.Description ?? site.Description;
+                        await _unitOfWork.Repository<Site>().UpdateAsync(site);
+                        await _unitOfWork.Commit(cancellationToken);
+                        return await Result<int>.SuccessAsync(site.Id, _localizer["Site Updated"]);
+                    }
+                    else
+                    {
+                        return await Result<int>.FailAsync("Site Not Found!");
+                    }
                 }
             }
         }
