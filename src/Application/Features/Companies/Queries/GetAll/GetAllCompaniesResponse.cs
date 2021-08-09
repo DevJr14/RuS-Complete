bbdcb7fbc5f4e73af3ec51410a5d8b5380ead840
Junior Matlou <Junianohmm6@gmail.com>
2021-08-09 15:07:46 +0200

    Added get all companies query without pagination and its response class.

---
+++ src/Application/Features/Companies/Queries/GetAll/GetAllCompaniesResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Companies.Queries.GetAll
+{
+    public class GetAllCompaniesResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortTitle { get; set; }
+
+        public string RegistrationNo { get; set; }
+
+        public DateTime? RegistrationDate { get; set; }
+    }
+}
