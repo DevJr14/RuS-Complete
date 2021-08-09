d461b5ccfea0658d0f9116ec202dc1adab6440d9
Junior Matlou <Junianohmm6@gmail.com>
2021-08-09 23:02:15 +0200

    Added AddEdit command and its validator

---
+++ src/Application/Validators/Features/Categories/AddEditCategoryCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RuS.Application.Features.Categories.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Validators.Features.Categories
+{
+    public class AddEditCategoryCommandValidator : AbstractValidator<AddEditCategoryCommand>
+    {
+        public AddEditCategoryCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Category name cannot be empty.")
+                .MaximumLength(30).WithMessage("Category name must not exceed 30 characters.");
+            RuleFor(c => c.Description)
+                .MaximumLength(60).WithMessage("Category name must not exceed 60 characters.");
+        }
+    }
+}
