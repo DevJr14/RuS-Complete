04c0926c00a303604a6c88b3054a8919754fdcff
Junior Matlou <Junianohmm6@gmail.com>
2021-08-09 16:04:15 +0200

    Adds Project entity

---
+++ src/Domain/Entities/Projects/Project.cs
@@ -0,0 +1,50 @@
+﻿using RuS.Domain.Contracts;
+using RuS.Domain.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Domain.Entities.Projects
+{
+    public class Project : AuditableEntity<int>
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int SiteId { get; set; }
+
+        public int StatusId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int PriorityId { get; set; }
+
+        public string Description { get; set; }
+
+        public string ScopeOfWork { get; set; }
+
+        public DateTime? Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        public DateTime? ActualStart { get; set; }
+
+        public DateTime? ActualEnd { get; set; }
+
+        public string Client { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        public string CellphoneNo { get; set; }
+
+        public string Email { get; set; }
+
+        public Site Site { get; set; }
+        public Status Status { get; set; }
+        public Category Category { get; set; }
+        public Priority Priority { get; set; }
+    }
+}
