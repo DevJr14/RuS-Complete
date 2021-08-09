de42ab468bca4ca9739f846e7bc288a720ee59cc
Junior Matlou <Junianohmm6@gmail.com>
2021-08-09 23:03:02 +0200

    Adds category repo and its implementation

---
+++ src/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IRepositoryAsync<Category, int> _repository;
+
+        public CategoryRepository(IRepositoryAsync<Category, int> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> IsUniqueEntry(string name, int id = 0)
+        {
+            List<Category> categories = await _repository.Entities.ToListAsync();
+            if (id == 0)
+            {
+                return !categories.Any(c => c.Name == name);
+            }
+            else
+            {
+                return !categories.Any(c => c.Name == name && c.Id != id);
+            }
+        }
+    }
+}
