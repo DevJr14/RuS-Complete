42a7db786207f71e59bc923d882dc8c496eac92f
Junior Matlou <Junianohmm6@gmail.com>
2021-08-11 22:38:49 +0200

    Added controllers with basic routes

---
+++ src/Server/Controllers/v1/Project/PrioritiesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RuS.Application.Features.Priorities.Commands;
+using RuS.Application.Features.Priorities.Queries;
+using RuS.Shared.Constants.Permission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuS.Server.Controllers.v1.Project
+{
+    public class PrioritiesController : BaseApiController<PrioritiesController>
+    {
+        /// <summary>
+        /// Get All Priorities
+        /// </summary>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Priorities.View)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var brands = await _mediator.Send(new GetAllPrioritiesQuery());
+            return Ok(brands);
+        }
+
+        /// <summary>
+        /// Get a Priority By Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status 200 Ok</returns>
+        [Authorize(Policy = Permissions.Categories.View)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var brand = await _mediator.Send(new GetPriorityByIdQuery() { Id = id });
+            return Ok(brand);
+        }
+
+        /// <summary>
+        /// Create/Update a Priority
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Categories.Create)]
+        [HttpPost]
+        public async Task<IActionResult> Post(AddEditPriorityCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Delete a Priority
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Priorities.Delete)]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeletePriorityCommand { Id = id }));
+        }
+    }
+}
