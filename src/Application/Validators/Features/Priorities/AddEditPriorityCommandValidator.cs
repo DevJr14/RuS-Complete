699ea443bc978a5b8eb19bceb055f2b1c5325fac
Junior Matlou <Junianohmm6@gmail.com>
2021-08-11 22:38:16 +0200

    Adds validator for command

---
+++ src/Application/Validators/Features/Priorities/AddEditPriorityCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RuS.Application.Features.Priorities.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Validators.Features.Priorities
+{
+    public class AddEditPriorityCommandValidator : AbstractValidator<AddEditPriorityCommand>
+    {
+        public AddEditPriorityCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Priority name cannot be empty.")
+                .MaximumLength(30).WithMessage("Priority name must not exceed 30 characters.");
+            RuleFor(p => p.Description)
+                .MaximumLength(60).WithMessage("Priority name must not exceed 60 characters.");
+        }
+    }
+}
