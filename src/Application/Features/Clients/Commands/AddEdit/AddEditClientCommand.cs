3810b178c383c9c934003dfa0fb9243de7b35b2c
Junior Matlou <Junianohmm6@gmail.com>
2021-08-13 21:24:55 +0200

    Adds client commands

---
+++ src/Application/Features/Clients/Commands/AddEdit/AddEditClientCommand.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Localization;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Clients.Commands.AddEdit
+{
+    public class AddEditClientCommand : IRequest<Result<int>>
+    {
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string TelephoneNo { get; set; }
+        public string CellphoneNo { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+    }
+
+    internal class AddEditClientCommandHandler : IRequestHandler<AddEditClientCommand, Result<int>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly IClientRepository _clientRepository;
+        private readonly IStringLocalizer<AddEditClientCommandHandler> _localizer;
+
+        public AddEditClientCommandHandler(IUnitOfWork<int> unitOfWork, IClientRepository clientRepository, IMapper mapper, IStringLocalizer<AddEditClientCommandHandler> localizer)
+        {
+            _unitOfWork = unitOfWork;
+            _clientRepository = clientRepository;
+            _mapper = mapper;
+            _localizer = localizer;
+        }
+
+        public async Task<Result<int>> Handle(AddEditClientCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/Features/Clients/Commands/Delete/DeleteClientCommand.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.Extensions.Localization;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Projects;
+using RuS.Shared.Wrapper;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Clients.Commands.Delete
+{
+    public class DeleteClientCommand : IRequest<Result<int>>
+    {
+        public int Id { get; set; }
+    }
+
+    internal class DeleteClientCommandHandler : IRequestHandler<DeleteClientCommand, Result<int>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly IStringLocalizer<DeleteClientCommandHandler> _localizer;
+
+        public DeleteClientCommandHandler(IUnitOfWork<int> unitOfWork, IStringLocalizer<DeleteClientCommandHandler> localizer)
+        {
+            _unitOfWork = unitOfWork;
+            _localizer = localizer;
+        }
+
+        public async Task<Result<int>> Handle(DeleteClientCommand command, CancellationToken cancellationToken)
+        {
+            var client = await _unitOfWork.Repository<Client>().GetByIdAsync(command.Id);
+            if (client != null)
+            {
+                await _unitOfWork.Repository<Client>().DeleteAsync(client);
+                await _unitOfWork.Commit(cancellationToken);
+                return await Result<int>.SuccessAsync(client.Id, _localizer["Client Deleted."]);
+            }
+            else
+            {
+                return await Result<int>.FailAsync(_localizer["Client Not Found!"]);
+            }
+        }
+    }
+}
