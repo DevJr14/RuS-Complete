33712cb3ae8830928ea07e623dda2c9768de13c9
Junior Matlou <Junianohmm6@gmail.com>
2021-08-13 21:25:27 +0200

    Added client queries and response client

---
+++ src/Application/Features/Clients/Queries/ClientResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Clients.Queries
+{
+    public class ClientResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string TelephoneNo { get; set; }
+        public string CellphoneNo { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ src/Application/Features/Clients/Queries/GetById/GetClientByIdQuery.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Projects;
+using RuS.Shared.Wrapper;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Features.Clients.Queries.GetById
+{
+    public class GetClientByIdQuery : IRequest<Result<ClientResponse>>
+    {
+        public int Id { get; set; }
+    }
+
+    internal class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, Result<ClientResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<int> _unitOfWork;
+        private readonly IStringLocalizer<GetClientByIdQueryHandler> _localizer;
+
+        public GetClientByIdQueryHandler(IUnitOfWork<int> unitOfWork, IMapper mapper, IStringLocalizer<GetClientByIdQueryHandler> localizer)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _localizer = localizer;
+        }
+
+        public async Task<Result<ClientResponse>> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+        {
+            var client = await _unitOfWork.Repository<Client>().Entities
+                .Where(c => c.Id == request.Id)
+                .FirstOrDefaultAsync();
+            if (client != null)
+            {
+                var mappedClient = _mapper.Map<ClientResponse>(client);
+                return await Result<ClientResponse>.SuccessAsync(mappedClient);
+            }
+            return await Result<ClientResponse>.FailAsync(_localizer[$"Client With Id: {request.Id} Not Found!"]);
+        }
+    }
+}
