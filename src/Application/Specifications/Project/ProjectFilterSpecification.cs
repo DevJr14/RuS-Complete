8d02d4ac2fd282332d4f5453730c0c7b64e4f621
Junior Matlou <Junianohmm6@gmail.com>
2021-08-14 02:14:49 +0200

    Added project specification

---
+++ src/Application/Specifications/Project/ProjectFilterSpecification.cs
@@ -0,0 +1,20 @@
+﻿using RuS.Application.Specifications.Base;
+
+namespace RuS.Application.Specifications.Project
+{
+    public class ProjectFilterSpecification : BaseSpecification<Domain.Entities.Projects.Project>
+    {
+        public ProjectFilterSpecification(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                Criteria = p => p.Name.Contains(searchString) || p.Description.Contains(searchString)
+                    || p.ScopeOfWork.Contains(searchString) || p.Code.Contains(searchString);
+            }
+            else
+            {
+                Criteria = c => true;
+            }
+        }
+    }
+}
