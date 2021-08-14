ad6b6bca687e062d422408dc978a113365fa4a72
Junior Matlou <Junianohmm6@gmail.com>
2021-08-14 10:49:29 +0200

    Updated validation rules

--- src/Application/Validators/Features/Companies/Commands/AddEdit/AddEditCompanyCommandValidator.cs
+++ src/Application/Validators/Features/Companies/Commands/AddEdit/AddEditCompanyCommandValidator.cs
@@ -11,7 +11,7 @@ namespace RuS.Application.Validators.Features.Companies.Commands.AddEdit
 
             RuleFor(c => c.Name)
                 .Must(c => !string.IsNullOrWhiteSpace(c)).WithMessage(c => localizer["Company Name is required"])
-                .MaximumLength(30).WithMessage(c => localizer["Company name must not exceed 30 characters."]);
+                .MaximumLength(30).WithMessage(c => localizer["Name must not exceed 30 characters."]);
         }
     }
 }
--- src/Application/Validators/Features/Employees/AddEditEmployeeCommandValidator.cs
+++ src/Application/Validators/Features/Employees/AddEditEmployeeCommandValidator.cs
@@ -14,14 +14,31 @@ namespace RuS.Application.Validators.Features.Employees
         public AddEditEmployeeCommandValidator(IStringLocalizer<AddEditEmployeeCommandValidator> localizer)
         {
             RuleFor(e => e.FirstName)
-                .Must(c => !string.IsNullOrWhiteSpace(c)).WithMessage(c => localizer["Firstname is required"])
-                .MaximumLength(30).WithMessage(c => localizer["Firstname must not exceed 30 characters."])
-                .Matches(@"^[a-zA-Z]+$").WithMessage(localizer["Firstname must contain alphabets only."]);
+                .Must(f => !string.IsNullOrWhiteSpace(f)).WithMessage(e => localizer["Firstname is required"])
+                .MaximumLength(30).WithMessage(e => localizer["Firstname must not exceed 30 characters."])
+                .Matches(@"^[a-zA-Z]+$").WithMessage(e => localizer["Firstname must contain alphabets only."]);
 
             RuleFor(e => e.LastName)
-                .Must(c => !string.IsNullOrWhiteSpace(c)).WithMessage(c => localizer["Lastname is required"])
-                .MaximumLength(30).WithMessage(c => localizer["Lastname must not exceed 30 characters."])
-                .Matches(@"^[a-zA-Z]+$").WithMessage(localizer["Lastname must contain alphabets only."]);
+                .Must(l => !string.IsNullOrWhiteSpace(l)).WithMessage(e => localizer["Lastname is required"])
+                .MaximumLength(30).WithMessage(e => localizer["Lastname must not exceed 30 characters."])
+                .Matches(@"^[a-zA-Z]+$").WithMessage(e => localizer["Lastname must contain alphabets only."]);
+
+            RuleFor(e => e.CompanyId)
+                .NotEqual(0).WithMessage(e => localizer["Employee must be linked to a Company"]);
+
+            RuleFor(e => e.CellphoneNo)
+                .Length(10).WithMessage(e => localizer["Cellphone no. must be 10 numbers long."])
+                .Matches(@"^[0-9]+$").WithMessage(e => localizer["Cellphone no. must contain numerics only."]);
+
+            RuleFor(e => e.Email)
+                .EmailAddress().WithMessage(e => localizer["Invalid email format."])
+                .MaximumLength(30).WithMessage(e => localizer["Email must not exceed 30 characters."]);
+
+            RuleFor(e => e.DateOfBirth)
+                .Must(d => d != default(DateTime)).WithMessage(e => localizer["Date of Birth is required"]);
+
+            RuleFor(e => e.Gender)
+                .Must(g => !string.IsNullOrWhiteSpace(g)).WithMessage(e => localizer["Gender is required"]);
         }
     }
 }
