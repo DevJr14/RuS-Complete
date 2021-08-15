321c4f7f409e23f4094d65eaa1ec71979fb5aba9
Junior Matlou <Junianohmm6@gmail.com>
2021-08-15 10:04:56 +0200

    Allowed spaces

--- src/Application/Validators/Features/Statuses/AddEditStatusCommandValidator.cs
+++ src/Application/Validators/Features/Statuses/AddEditStatusCommandValidator.cs
@@ -14,7 +14,7 @@ namespace RuS.Application.Validators.Features.Statuses
         public AddEditStatusCommandValidator(IStringLocalizer<AddEditStatusCommandValidator> localizer)
         {
             RuleFor(s => s.Name)
-                .Must(s => !string.IsNullOrWhiteSpace(s)).WithMessage(s => localizer["Name is required!"])
+                .Must(s => !string.IsNullOrEmpty(s)).WithMessage(s => localizer["Name is required!"])
                 .MaximumLength(30).WithMessage(s => localizer["Status name must not exceed 30 characters."]);
             RuleFor(s => s.Description)
                 .MaximumLength(60).WithMessage(s => localizer["Status name must not exceed 60 characters."]);
