642eb6dc11164455f2a9a6f2c0a2b88c41473efc
Junior Matlou <Junianohmm6@gmail.com>
2021-08-15 09:57:22 +0200

    Completed AddEdit client command logic

--- src/Application/Features/Clients/Commands/AddEdit/AddEditClientCommand.cs
+++ src/Application/Features/Clients/Commands/AddEdit/AddEditClientCommand.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Localization;
 using RuS.Application.Interfaces.Repositories;
+using RuS.Domain.Entities.Projects;
 using RuS.Shared.Wrapper;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ namespace RuS.Application.Features.Clients.Commands.AddEdit
 {
     public class AddEditClientCommand : IRequest<Result<int>>
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string ContactPerson { get; set; }
         public string TelephoneNo { get; set; }
@@ -42,7 +44,51 @@ namespace RuS.Application.Features.Clients.Commands.AddEdit
 
         public async Task<Result<int>> Handle(AddEditClientCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (command.Id == 0)
+            {
+                var notUnique = await _clientRepository.IsUniqueEntry(command);
+                if (notUnique)
+                {
+                    return await Result<int>.FailAsync(_localizer["Client already exists."]);
+                }
+                else
+                {
+                    var client = _mapper.Map<Client>(command);
+                    await _unitOfWork.Repository<Client>().AddAsync(client);
+                    await _unitOfWork.Commit(cancellationToken);
+                    return await Result<int>.SuccessAsync(client.Id, _localizer["Client Saved"]);
+                }
+            }
+            else
+            {
+                var notUnique = await _clientRepository.IsUniqueEntry(command);
+                if (notUnique)
+                {
+                    return await Result<int>.FailAsync(_localizer["Client already exists."]);
+                }
+                var client = await _unitOfWork.Repository<Client>().GetByIdAsync(command.Id);
+                if (client != null)
+                {
+                    client.Name = command.Name;
+
+                    client.ContactPerson = command.ContactPerson ?? client.ContactPerson;
+                    client.CellphoneNo = command.CellphoneNo ?? client.CellphoneNo;
+                    client.TelephoneNo = command.TelephoneNo ?? client.TelephoneNo;
+                    client.Street = command.Street ?? client.Street;
+                    client.Suburb = command.Suburb ?? client.Suburb;
+                    client.City = command.City ?? client.City;
+                    client.Postcode = command.Postcode ?? client.Postcode;
+                    client.Email = command.Email ?? client.Email;
+
+                    await _unitOfWork.Repository<Client>().UpdateAsync(client);
+                    await _unitOfWork.Commit(cancellationToken);
+                    return await Result<int>.SuccessAsync(client.Id, _localizer["Client Updated"]);
+                }
+                else
+                {
+                    return await Result<int>.FailAsync(_localizer["Client Not Found!"]);
+                }
+            }
         }
     }
 }
