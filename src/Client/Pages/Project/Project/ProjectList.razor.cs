606e78b5a4a56df1201686b3009c861c711caeb3
Junior Matlou <Junianohmm6@gmail.com>
2021-08-19 22:27:48 +0200

    Added project list and its data provider

---
+++ src/Client/Pages/Project/Project/ProjectList.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using RuS.Application.Features.Projects.Queries;
+using RuS.Client.Infrastructure.Managers.Project.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Pages.Project.Project
+{
+    public partial class ProjectList
+    {
+        [Inject] private IProjectManager ProjectManager { get; set; }
+        public List<ProjectResponse> _projectList { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetProjectsAsync();
+        }
+
+        private async Task GetProjectsAsync()
+        {
+            var response = await ProjectManager.GetAllAsync();
+            if (response.Succeeded)
+            {
+                _projectList = response.Data.ToList();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }        
+    }
+}
