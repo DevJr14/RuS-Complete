53c388f18ae44f518fbf71b792b9aef5fb0cd26d
Junior Matlou <Junianohmm6@gmail.com>
2021-08-26 22:17:34 +0200

    Added task response class

---
+++ src/Application/Features/Tasks/Queries/TaskResponse.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using RuS.Application.Features.Categories.Queries;
+using RuS.Application.Features.Priorities.Queries;
+using RuS.Application.Features.Projects.Queries;
+using RuS.Application.Features.Statuses.Queries;
+using RuS.Application.Mappings;
+using RuS.Domain.Entities.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RuS.Application.Features.Tasks.Queries
+{
+    public class TaskResponse : IMapFrom<Task>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int? StatusId { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public int? PriorityId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        public ProjectResponse Project { get; set; }
+        public StatusResponse Status { get; set; }
+        public CategoryResponse Category { get; set; }
+        public PriorityResponse Priority { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<TaskResponse, Task>().ReverseMap();
+        }
+    }
+}
