9cc24e56215139378b243490d8fa00024d5ff538
Junior Matlou <Junianohmm6@gmail.com>
2021-08-26 23:14:03 +0200

    Added Task manager and its implemetation

---
+++ src/Client.Infrastructure/Managers/Project/Task/ITaskManager.cs
@@ -0,0 +1,23 @@
+﻿using RuS.Application.Features.Tasks.Commands.AddEdit;
+using RuS.Application.Features.Tasks.Queries;
+using RuS.Shared.Wrapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Infrastructure.Managers.Project.Task
+{
+    interface ITaskManager : IManager
+    {
+        Task<IResult<List<TaskResponse>>> GetAllAsync();
+
+        Task<IResult<List<TaskResponse>>> GetAllForProjectAsync(int projectId);
+
+        Task<IResult<int>> SaveAsync(AddEditTaskCommand command);
+
+        Task<IResult<int>> DeleteAsync(int id);
+
+        Task<IResult<string>> ExportToExcelAsync(string searchString = "");
+
+        Task<IResult<TaskResponse>> GetByIdAsync(int id);
+    }
+}
---
+++ src/Client.Infrastructure/Managers/Project/Task/TaskManager.cs
@@ -0,0 +1,59 @@
+﻿using RuS.Application.Features.Tasks.Commands.AddEdit;
+using RuS.Application.Features.Tasks.Queries;
+using RuS.Client.Infrastructure.Extensions;
+using RuS.Shared.Wrapper;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Infrastructure.Managers.Project.Task
+{
+    public class TaskManager : ITaskManager
+    {
+        private readonly HttpClient _httpClient;
+
+        public TaskManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IResult<int>> DeleteAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{Routes.TaskEndpoints.Delete}/{id}");
+            return await response.ToResult<int>();
+        }
+
+        public async Task<IResult<string>> ExportToExcelAsync(string searchString = "")
+        {
+            var response = await _httpClient.GetAsync(string.IsNullOrWhiteSpace(searchString)
+                ? Routes.TaskEndpoints.Export
+                : Routes.TaskEndpoints.ExportFiltered(searchString));
+            return await response.ToResult<string>();
+        }
+
+        public async Task<IResult<List<TaskResponse>>> GetAllAsync()
+        {
+            var response = await _httpClient.GetAsync(Routes.TaskEndpoints.GetAll);
+            return await response.ToResult<List<TaskResponse>>();
+        }
+
+        public async Task<IResult<List<TaskResponse>>> GetAllForProjectAsync(int projectId)
+        {
+            var response = await _httpClient.GetAsync(Routes.TaskEndpoints.GetAllForProject(projectId));
+            return await response.ToResult<List<TaskResponse>>();
+        }
+
+        public async Task<IResult<TaskResponse>> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync(Routes.TaskEndpoints.GetById(id));
+            return await response.ToResult<TaskResponse>();
+        }
+
+        public async Task<IResult<int>> SaveAsync(AddEditTaskCommand command)
+        {
+            var response = await _httpClient.PostAsJsonAsync(Routes.TaskEndpoints.Save, command);
+            return await response.ToResult<int>();
+        }
+    }
+}
