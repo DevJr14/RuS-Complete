0eafb13b1d579cc965023c2ae8b3ac53852d1ece
Junior Matlou <Junianohmm6@gmail.com>
2021-08-26 22:18:55 +0200

    Added Task repo, its implementation and registered them to service container

---
+++ src/Application/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using RuS.Application.Features.Tasks.Commands.AddEdit;
+using System.Threading.Tasks;
+
+namespace RuS.Application.Interfaces.Repositories
+{
+    public interface ITaskRepository
+    {
+        string GenerateProjectCode();
+
+        Task<bool> IsUniqueEntry(AddEditTaskCommand command);
+    }
+}
