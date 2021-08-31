b6e0402a67ad59785df29c6a4472e7efe8ebf33a
Junior Matlou <Junianohmm6@gmail.com>
2021-08-31 21:24:03 +0200

    Removed nullable

--- src/Application/Features/Tasks/Commands/AddEdit/AddEditTaskCommand.cs
+++ src/Application/Features/Tasks/Commands/AddEdit/AddEditTaskCommand.cs
@@ -26,11 +26,11 @@ namespace RuS.Application.Features.Tasks.Commands.AddEdit
 
         public int ProjectId { get; set; }
 
-        public int? StatusId { get; set; }
+        public int StatusId { get; set; }
 
-        public int? CategoryId { get; set; }
+        public int CategoryId { get; set; }
 
-        public int? PriorityId { get; set; }
+        public int PriorityId { get; set; }
 
         public string Description { get; set; }
 
