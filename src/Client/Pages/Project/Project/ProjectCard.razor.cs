6bed2876198d1b2732d4cbcc654188b407e609b9
Junior Matlou <Junianohmm6@gmail.com>
2021-09-04 00:38:11 +0200

    Updated to use EventCallbacks to parent component

--- src/Client/Pages/Project/Project/ProjectCard.razor.cs
+++ src/Client/Pages/Project/Project/ProjectCard.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using RuS.Application.Features.Projects.Queries;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,23 @@ namespace RuS.Client.Pages.Project.Project
     public partial class ProjectCard
     {
         [Parameter] public ProjectResponse Project { get; set; }
-        [Parameter] public int MyProperty { get; set; }
 
-        private void Details()
+        [Parameter] public EventCallback<int> OnEditSelection { get; set; }
+        [Parameter] public EventCallback<int> OnDeleteSelection { get; set; }
+
+        private void Details(int id)
         {
-            _navigationManager.NavigateTo("/project/projects/add");
+            _navigationManager.NavigateTo($"/project/projects/edit/{id}");
+        }
+
+        protected async Task DeleteSelectionChanged(MouseEventArgs e, int projectId)
+        {
+            await OnDeleteSelection.InvokeAsync(projectId);
+        }
+
+        protected async Task EditProjectSelectionChanged(MouseEventArgs e, int id)
+        {
+            await OnEditSelection.InvokeAsync(id);           
         }
     }
 }
