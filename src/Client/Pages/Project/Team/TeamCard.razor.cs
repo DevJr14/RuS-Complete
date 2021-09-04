ef63cc9f9490f28f3044913087b1b3c420079632
Junior Matlou <Junianohmm6@gmail.com>
2021-09-05 00:47:01 +0200

    Aded team card component

---
+++ src/Client/Pages/Project/Team/TeamCard.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using RuS.Application.Features.Teams.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Pages.Project.Team
+{
+    public partial class TeamCard
+    {
+        [Parameter]
+        public TeamResponse Team { get; set; }
+
+        [Parameter] public EventCallback<int> OnEditSelection { get; set; }
+        [Parameter] public EventCallback<int> OnDeleteSelection { get; set; }
+
+        protected async Task DeleteSelectionChanged(MouseEventArgs e, int teamId)
+        {
+            await OnDeleteSelection.InvokeAsync(teamId);
+        }
+
+        protected async Task EditTeamSelectionChanged(MouseEventArgs e, int id)
+        {
+            await OnEditSelection.InvokeAsync(id);
+        }
+    }
+}
