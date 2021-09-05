0a64af6482d8a3a524fb839885b90197bb3129f5
Junior Matlou <Junianohmm6@gmail.com>
2021-09-05 20:01:09 +0200

    Added team members ends points

--- src/Client.Infrastructure/Routes/TeamEndpoints.cs
+++ src/Client.Infrastructure/Routes/TeamEndpoints.cs
@@ -17,10 +17,16 @@
             return $"api/v1/teams/project/{projectId}";
         }
 
+        public static string GetTeamMembers(int teamId)
+        {
+            return $"api/v1/teams/team-members/{teamId}";
+        }
+
         public static string Export = "api/v1/teams/export";
         public static string GetAll = "api/v1/teams";
         public static string Delete = "api/v1/teams";
         public static string Save = "api/v1/teams";
         public static string SaveMembers = "api/v1/teams/members";
+        public static string UpdateTeamMembers = "api/v1/teams/update-members";
     }
 }
