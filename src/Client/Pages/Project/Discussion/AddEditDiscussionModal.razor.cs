cfc4bb712e79438d7d48dba2af7be2bb25c91e46
Junior Matlou <Junianohmm6@gmail.com>
2021-12-08 15:10:29 +0200

    Add Add or Edit Discussion dialog

---
+++ src/Client/Pages/Project/Discussion/AddEditDiscussionModal.razor.cs
@@ -0,0 +1,42 @@
+﻿using Blazored.FluentValidation;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using RuS.Application.Features.Discussions.Commands.AddEdit;
+using RuS.Client.Infrastructure.Managers.Project.Discussion;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Pages.Project.Discussion
+{
+    public partial class AddEditDiscussionModal
+    {
+        [Inject] public IDiscussionManager DiscussionManager { get; set; }
+        [Parameter] public AddEditDiscussionCommand _command { get; set; } = new();
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+
+        private FluentValidationValidator _fluentValidationValidator;
+        private bool Validated => _fluentValidationValidator.Validate(options => { options.IncludeAllRuleSets(); });
+
+        private async Task SaveAsync()
+        {
+            var response = await DiscussionManager.SaveAsync(_command);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+    }
+}
