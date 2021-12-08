17994758254ddbf18344380f1cab85dadebfadcf
Junior Matlou <Junianohmm6@gmail.com>
2021-12-08 13:49:25 +0200

    Discussion response client data encapsulation

---
+++ src/Application/Features/Discussions/Queries/DiscussionResponse.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using RuS.Application.Features.Projects.Queries;
+using RuS.Application.Features.Tasks.Queries;
+using RuS.Application.Mappings;
+using RuS.Domain.Entities.Projects;
+
+namespace RuS.Application.Features.Discussions.Queries
+{
+    public class DiscussionResponse : IMapFrom<Discussion>
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public int ProjectId { get; set; }
+        public int? TaskId { get; set; }
+        public ProjectResponse Project { get; set; } = new();
+        public TaskResponse Task { get; set; } = new();
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Discussion, DiscussionResponse>();
+        }
+    }
+}
