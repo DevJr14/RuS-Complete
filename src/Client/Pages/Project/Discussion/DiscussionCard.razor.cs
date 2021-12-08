79131b25ac5e5a9f2108ba33108c03b984bc3aa0
Junior Matlou <Junianohmm6@gmail.com>
2021-12-08 15:10:02 +0200

    Added discussion card component and its provider.

---
+++ src/Client/Pages/Project/Discussion/DiscussionCard.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using RuS.Application.Features.Discussions.Queries;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Pages.Project.Discussion
+{
+    public partial class DiscussionCard
+    {
+        [Parameter] public DiscussionResponse Discussion { get; set; }
+        [Parameter] public EventCallback<int> OnEditSelection { get; set; }
+        [Parameter] public EventCallback<int> OnDeleteSelection { get; set; }
+
+        protected async Task DeleteSelectionChanged(MouseEventArgs e, int tastId)
+        {
+            await OnDeleteSelection.InvokeAsync(tastId);
+        }
+
+        protected async Task EditSelectionChanged(MouseEventArgs e, int id)
+        {
+            await OnEditSelection.InvokeAsync(id);
+        }
+    }
+}
