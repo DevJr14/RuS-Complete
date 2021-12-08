e691a3e6e302659e6fa1e66d85141a3698a3dcd5
Junior Matlou <Junianohmm6@gmail.com>
2021-12-08 15:01:29 +0200

    Method to call API endpoints for Discussion via httpClient

---
+++ src/Client.Infrastructure/Managers/Project/Discussion/IDiscussionManager.cs
@@ -0,0 +1,21 @@
+﻿using RuS.Application.Features.Discussions.Commands.AddEdit;
+using RuS.Application.Features.Discussions.Queries;
+using RuS.Shared.Wrapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RuS.Client.Infrastructure.Managers.Project.Discussion
+{
+    public interface IDiscussionManager : IManager
+    {
+        Task<IResult<List<DiscussionResponse>>> GetAllForTaskAsync(int taskId);
+
+        Task<IResult<List<DiscussionResponse>>> GetAllForProjectAsync(int projectId);
+
+        Task<IResult<int>> SaveAsync(AddEditDiscussionCommand command);
+
+        Task<IResult<int>> DeleteAsync(int id);
+
+        Task<IResult<DiscussionResponse>> GetByIdAsync(int id);
+    }
+}
